19e265db9aeb462b79a9738a03c8bcfa13bdc848
Peyton McNully <peyton.mcnully@gmail.com>
2011-03-07 16:57:25 -0500

    add bp

--- base.rb
+++ base.rb
@@ -13,10 +13,37 @@ run "curl -L http://code.jquery.com/jquery-1.4.2.min.js > public/javascripts/jqu
 run "curl -L http://github.com/malsup/form/raw/master/jquery.form.js?v2.43 > public/javascripts/jquery.form.js"
 
 # Install blueprint 
-run "git clone https://peytonmcnully@github.com/peytonmcnully/blueprint-css.git blueprint > public/stylesheets/"
+run "git clone https://peytonmcnully@github.com/peytonmcnully/blueprint-css.git public/stylesheets/blueprint"
  
 # Cleans up a lot of the files that might clutter your app when you start.
 run "rm README"
 run "rm public/index.html"
 run "rm public/favicon.ico"
 run "rm public/robots.txt"
+
+generate :controller, "main index"
+
+file 'app/views/layouts/main.html.erb', 
+%q{<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+  <title>App Title</title>
+	<%= stylesheet_link_tag 'style', :media => 'screen, projection' %>
+	<%= stylesheet_link_tag 'blueprint/screen.css', :media => 'screen, projection' %>
+	<%= stylesheet_link_tag 'blueprint/print.css', :media => 'print' %>
+	<!--[if lt IE 8]> <%= stylesheet_link_tag 'blueprint/ie.css', :media => 'screen, projection' %> <![endif]-->
+</head>
+<body>
+	<div class="container">
+		<p class="span-24 last" style="color: green"><%= flash[:notice] %></p>
+		<%= yield %>
+	</div>
+</body>
+</html>
+}
+file 'public/stylesheets/style.css',
+%q{
+}
